1be082138e117ba88f696fd38ec90c839d9e68ce
Carlo <carlo.namoca@gmail.com>
2017-10-07 17:04:45 -0700

    Initial Commit

---
+++ Snakes and Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snakes and Ladders
+//
+//  Created by Carlo Namoca on 2017-10-07.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
